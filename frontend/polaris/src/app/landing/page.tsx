622b68023c6a7dcaac6102f7b8d97cb88b888cf3
Krakqn <139305417+Krakqn@users.noreply.github.com>
2024-09-15 09:56:09 -0400

    Finished landing page

--- frontend/polaris/src/app/landing/page.tsx
+++ frontend/polaris/src/app/landing/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react';
+import Image from 'next/image';
 import { useAuth, SignInButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
@@ -15,6 +16,16 @@ export default function LandingPage() {
 
   return (
     <div className="max-w-3xl mx-auto p-8 text-center font-sans">
+      <div className="flex justify-center mb-6">
+        <Image
+          src="https://cdn.discordapp.com/attachments/1283171026032918549/1284870280539410473/Screenshot_from_2024-09-15_15-32-28.png?ex=66e83432&is=66e6e2b2&hm=21416872a9b9e5929676525520f4898af110b3d81459f0e70eeae62359702900&"
+          alt="Polaris Logo"
+          width={200}
+          height={200}
+          objectFit="cover"
+        />
+      </div>
+
       <div className="bg-blue-100 text-blue-800 px-3 py-1 rounded-full inline-block mb-4">
         Master the Market
       </div>
@@ -22,8 +33,10 @@ export default function LandingPage() {
         <span className="italic">Let</span> the <span className="font-bold">North Star</span> guide your financial decisions
       </h1>
       <p className="text-xl mb-6 text-gray-500">
-        In this step-by-step course, learn the exact settings and techniques to capture amazing photos
-        with your iPhone. No experience needed — just follow the steps and see the difference!
+      Polaris is a real-time market analysis tool that leverages an agentic LLM 
+      with RAG to align financial data with sentiment and frequency analysis on 
+      news and social media, thereby offering crucial investment insights and a 
+      helpful advisor feature for user support.
       </p>
       {!isSignedIn && (
         <SignInButton mode="redirect">
@@ -37,8 +50,12 @@ export default function LandingPage() {
           Continue to Dashboard
         </Button>
       )}
-      
-      <p className="text-sm text-gray-600 mt-2">Limited time only launch price.</p>
+
+        {isSignedIn ? (
+        <p className="text-sm text-gray-600 mt-2">You are already signed in!</p>
+            ) : (
+        <p className="text-sm text-gray-600 mt-2">Let us guide you!</p>
+        )}
     </div>
   );
 }
